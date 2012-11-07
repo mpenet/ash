0c44e893dc44d64485dc5cdf39c31e2e2dbaad1b
Max Penet <m@qbits.cc>
2012-11-07 16:16:22 +0100

    add wolfram plugin

---
+++ src/ash/plugins/wolfram.clj
@@ -0,0 +1,37 @@
+(ns ash.plugins.wolfram
+  (:require
+   [ash.bot :as irc]
+   [clj-http.client :as client]
+   [clojure.xml :as xml]
+   [clojure.zip :as zip]))
+
+(defn search
+  [app-id term]
+  (if-let [result (-> (client/get (format "http://api.wolframalpha.com/v2/query?"
+                                       term app-id)
+                               {:query-params {"input" term
+                                               "appid" app-id}
+                                :throw-exceptions false})
+
+                   :body
+                   .getBytes
+                   (java.io.ByteArrayInputStream.)
+                   xml/parse
+                   zip/xml-zip
+                   first
+                   :content
+                   second
+                   :content
+                   first
+                   :content
+                   first
+                   :content
+                   first)]
+    (format "%s ⇒ http://www.wolframalpha.com/input/?i=%s" result term)
+    (format "No result for %s" term)))
+
+(defn handler [bot app-id]
+  (irc/listen bot :on-message
+              (fn [event]
+                (when-let [t (second (re-find #"^\?w\s+(.+)" (:content event)))]
+                  (irc/reply bot event (search app-id t) true)))))
