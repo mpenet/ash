5e9e2c2e81f34ec67568da5976c95ff6a63786ba
Max Penet <m@qbits.cc>
2012-11-17 23:41:15 +0100

    remove useless import

--- src/qbits/ash/store.clj
+++ src/qbits/ash/store.clj
@@ -1,7 +1,5 @@
 (ns qbits.ash.store
-  (:import
-   [java.security MessageDigest]
-   [org.mapdb DB DBMaker]))
+  (:import [org.mapdb DB DBMaker]))
 
 (defonce db (-> (DBMaker/newFileDB (java.io.File. "brain"))
                 .closeOnJvmShutdown
