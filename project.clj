f282f415fd834d711dab8892f8ae9b03b1b87b03
Max Penet <m@qbits.cc>
2012-11-17 18:32:31 +0100

    add fact plugin

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject cc.qbits/ash "0.2.1"
   :description "clojure irc client based on pircbotx, originally forked from clj-irc"
+  :repositories [["sonatype" "https://oss.sonatype.org/content/repositories/snapshots/"]]
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.pircbotx/pircbotx "1.7"]
@@ -9,7 +10,7 @@
                  [clojail "1.0.3"]
                  [clj-http "0.5.7"]
                  [org.jsoup/jsoup "1.6.3"]
-                 ;; [org.mapdb/mapdb "0.9-SNAPSHOT"]
+                 [org.mapdb/mapdb "0.9-SNAPSHOT"]
                  ]
   ;; :warn-on-reflection true
   )
--- src/qbits/ash/plugins/facts.clj
+++ src/qbits/ash/plugins/facts.clj
@@ -1,2 +1,50 @@
 (ns qbits.ash.plugins.facts
-  (:require [qbits.ash :as ash]))
+  (:require
+   [qbits.ash :as ash]
+   [clojure.string :as string])
+  (:import
+   [java.security MessageDigest]
+   [org.mapdb DB DBMaker]))
+
+(defn make-id
+  [fact]
+  (apply str (map #(format "%02x" (bit-and % 0xff))
+                  (.digest
+                   (doto (MessageDigest/getInstance "MD5")
+                     (.update (.getBytes (-> fact string/trim string/lower-case))))))))
+
+;; to be moved elsewhere if we need more of this
+(defonce db (-> (DBMaker/newFileDB (java.io.File. "brain"))
+                .closeOnJvmShutdown
+                .make))
+
+(defonce facts (.getTreeMap db "facts"))
+
+(defn put
+  ""
+  [trigger fact]
+  (.put facts (make-id trigger) fact)
+  (.commit db))
+
+(defn fetch
+  ""
+  [trigger]
+  (.get facts (make-id trigger)))
+
+(defn handler
+  [bot]
+  ;;ask
+  (ash/listen bot :on-message
+              (fn [event]
+                (when-let [fact (second (re-find
+                                         (re-pattern
+                                          (format "%s\\s*\\:(.+)"
+                                                  (.getName bot)))
+                                         (:content event)))]
+                  (ash/reply bot event (fetch fact)))))
+  ;; store
+  (ash/listen bot :on-message
+              (fn [event]
+                (when-let [fact (re-find #"^fact!!(.+) (.+)")]
+                  (ash/reply bot event (put (first fact)
+                                            (second fact)))))))
