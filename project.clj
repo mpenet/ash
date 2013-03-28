2283a8216a1eb158f249d2f061070457f91bbc4b
Max Penet <m@qbits.cc>
2013-03-28 13:05:53 +0100

    target clj 1.5.1

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject cc.qbits/ash "0.2.5"
   :description "clojure irc client based on pircbotx, originally forked from clj-irc"
   :repositories [["sonatype" "https://oss.sonatype.org/content/repositories/snapshots/"]]
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.pircbotx/pircbotx "1.8"]
 
